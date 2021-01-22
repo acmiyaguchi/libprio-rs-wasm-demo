63c676394d68a57d41e3bddf3befb432a9adcc71
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-01-22 10:07:31 -0800

    Add simple_encode (which fails)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 mod utils;
 
+use prio::client;
+use prio::encrypt;
+use prio::finite_field;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,11 +14,32 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, libprio-wasm!");
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ClientShares {
+    pub a: Vec<u8>,
+    pub b: Vec<u8>,
+}
+
+#[wasm_bindgen]
+pub fn client_encode_simple(data: &[u32], public_key1: &str, public_key2: &str) -> JsValue {
+    let pub_key1 = encrypt::PublicKey::from_base64(public_key1).unwrap();
+    let pub_key2 = encrypt::PublicKey::from_base64(public_key2).unwrap();
+
+    let data = data
+        .iter()
+        .map(|x| finite_field::Field::from(*x))
+        .collect::<Vec<finite_field::Field>>();
+    let encoded_shares = client::encode_simple(&data, pub_key1, pub_key2);
+    let (a, b) = encoded_shares.unwrap();
+    let shares = ClientShares { a: a, b: b };
+    JsValue::from_serde(&shares).unwrap()
 }
--- tests/web.rs
+++ tests/web.rs
@@ -4,10 +4,16 @@
 
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
+use libprio_wasm::client_encode_simple;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
-    assert_eq!(1 + 1, 2);
+    let pub_key1 =
+        "BIl6j+J6dYttxALdjISDv6ZI4/VWVEhUzaS05LgrsfswmbLOgNt9HUC2E0w+9RqZx3XMkdEHBHfNuCSMpOwofVQ=";
+    let pub_key2 =
+        "BNNOqoU54GPo+1gTPv+hCgA9U2ZCKd76yOMrWa1xTWgeb4LhFLMQIQoRwDVaW64g/WTdcxT4rDULoycUNFB60LE=";
+    let data_u32 = [0u32, 1, 0, 1, 1, 0, 0, 0, 1];
+    let res = client_encode_simple(&data_u32, pub_key1, pub_key2);
 }
