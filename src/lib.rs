08dade45fd6ec61f43b86c2673e9d5cbdab52d97
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-02-17 19:21:08 -0800

    Add a simple test of rust library

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,18 @@
 mod utils;
 
-use prio::client;
-use prio::encrypt;
-use prio::finite_field;
+
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::prelude::*;
+use prio::client;
+use prio::encrypt;
+use prio::finite_field;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, libprio-wasm!");
-}
 
 #[derive(Serialize, Deserialize)]
 pub struct ClientShares {
@@ -39,6 +30,7 @@ pub fn client_encode_simple(data: &[u32], public_key1: &str, public_key2: &str) 
         .map(|x| finite_field::Field::from(*x))
         .collect::<Vec<finite_field::Field>>();
     let encoded_shares = client::encode_simple(&data, pub_key1, pub_key2);
+    assert_eq!(encoded_shares.is_some(), true);
     let (a, b) = encoded_shares.unwrap();
     let shares = ClientShares { a: a, b: b };
     JsValue::from_serde(&shares).unwrap()
